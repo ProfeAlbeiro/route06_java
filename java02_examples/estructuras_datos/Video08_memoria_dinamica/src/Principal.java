49a2fa55b0befc89f67d9a0513b16a1119dcb54a
Thomas-2024 <tomllgyy40@gmail.com>
2024-11-15 18:02:21 -0500

    Estructura de datos

--- java02_examples/estructuras_datos/Video08_memoria_dinamica/src/Principal.java
+++ java02_examples/estructuras_datos/Video08_memoria_dinamica/src/Principal.java
@@ -1,8 +1,22 @@
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+
 public class Principal {
     
     public static void main(String[] args) {
         
-    }
-    
+        ArrayList<Integer> listaNumerica = new ArrayList<Integer>(); 
+        int n,i=0;
+        do {            
+            n = Integer.parseInt(JOptionPane.showInputDialog(
+                    null, "Ingrese el número " + (i + 1) + ":"));
+            listaNumerica.add(n);
+            i++;
+        } while (n>=0);
+        for (i = 0; i < listaNumerica.size(); i++) {
+            System.out.println("Elemento " + (i + 1) + ": " + listaNumerica.get(i));
+        }
+    }    
 }
